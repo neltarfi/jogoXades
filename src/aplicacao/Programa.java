37456a79df71f2f8ffcf518eaf2d254beebaf402
Nelson Tarnoschi Filho <nelson759734@gmail.com>
2019-06-05 09:51:55 -0300

    Primeira classe: Posicao

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,10 +1,12 @@
 package aplicacao;
 
+import jogoTabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
 	}
 
 }
