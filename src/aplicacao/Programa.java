13a089cd0cc8cfed1f46fa24f20f12c2620e344a
Nelson Tarnoschi Filho <nelson759734@gmail.com>
2019-06-05 10:33:53 -0300

    Iniciando a implementação do Tabuleiro e Peça

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,12 +1,12 @@
 package aplicacao;
 
-import jogoTabuleiro.Posicao;
+import jogoTabuleiro.Tabuleiro;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		Posicao pos = new Posicao(3, 5);
-		System.out.println(pos);
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+		
 	}
 
 }
